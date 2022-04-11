8bb79cc0322e2ab6773cd414287c83d9191d35fa
Aryangandhi07 <103401791+Aryangandhi07@users.noreply.github.com>
2022-04-11 09:42:48 +0530

    Create exp1.py

---
+++ ARYAN_21BTAI06/exp1.py
@@ -0,0 +1,9 @@
+a=int(input("enter a integer : "))
+b=input("enter a string : ")
+c=float(input("enter a number(float) : "))
+
+print(a,"\t",b,"\t",c)
+
+print(type(a))
+print(type(b))
+print(type(c))
